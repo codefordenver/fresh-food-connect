faa45d2e76529c6667ae922871f03d1f252798ef
Aiko Cheslin <lucidpaper@gmail.com>
2015-08-10 14:32:45 -0600

    changed map locations

--- ffc/client/map/map.js
+++ ffc/client/map/map.js
@@ -45,7 +45,7 @@ if (Meteor.isClient) {
     mapOptions: function() {
       if (GoogleMaps.loaded()) {
         return {
-          center: new google.maps.LatLng(-37.8136, 144.9631),
+          center: new google.maps.LatLng(39.7392, -104.9903),
           zoom: 8
         };
       }
