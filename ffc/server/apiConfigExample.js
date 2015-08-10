be415c61e02b99fa522cabb36c918a24cd1b545e
laxonmind <lucidpaper@gmail.com>
2015-08-10 15:29:17 -0600

    added apiConfig file for geolocation api

---
+++ ffc/server/apiConfigExample.js
@@ -0,0 +1,8 @@
+//this is an example of how to set up the apiConfig.js file
+//make sure that in your local repos .git/info/exclude it includes
+// ffc/server/apiConfig.js
+//this will exclude this file from git commits and will protect your api key
+//make a file called apiConfig.js in the same folder as this file
+//   in apiConfig.js put
+//   var googleGeoCodeApiKey = 123yourApiKey123;
+// googleGeoCodeApiKey will be reffrenced elsewhere in the server code
