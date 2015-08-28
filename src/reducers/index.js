cb4efd97e741632430cbc76b48e4dfde3a2bc25f
Willy Douglas <willygdouglas@gmail.com>
2015-08-28 17:20:25 -0600

    Start from scratch

--- src/reducers/index.js
+++
@@ -1,4 +0,0 @@
-import {createFormReducer} from 'redux-form';
-export info from './info';
-export auth from './auth';
-export const survey = createFormReducer('survey', ['name', 'email', 'occupation']);
--- src/validation/loginValidation.js
+++
@@ -1,8 +0,0 @@
-import {createValidator, required, maxLength, email} from './validation';
-
-const loginValidation = createValidator({
-  email: [required, email],
-  password: [required, maxLength(50)]
-});
-
-export default loginValidation;
