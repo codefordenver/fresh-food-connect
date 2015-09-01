b04553639fd6170e52a1a2dc685031778e0ff2a3
dviramontes <dviramontes@levelsbeyond.com>
2015-08-31 23:50:37 -0600

    fixes img include in Navbar.js

--- webpack.config.js
+++ webpack.config.js
@@ -38,9 +38,9 @@ module.exports = {
         test: /\.scss$/,
         loader: 'style!css!sass'
       },
-      { test: /\.png$/, loader: "url-loader?mimetype=image/png" },
-      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!cssnext-loader') },
-      { test: /\.js$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ }
+      {test: /\.png$/, loader: "url-loader?mimetype=image/png"},
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!cssnext-loader')},
+      {test: /\.js$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/}
     ]
   },
   cssnext: {
