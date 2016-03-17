6309470aa792ade24265b06c90e4130164b47880
Anthony Navarre <anthony@thoughtbot.com>
2016-03-16 19:16:36 -0600

    Provide Procfile for running web process on Heroku
    Also provides production express server

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
-var path = require('path');
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+if (process.env.NODE_ENV === 'production') {
+  var express = require('express'),
+      port = (process.env.PORT || 8080),
+      app = express()
 
-new WebpackDevServer(webpack(config), {
-  contentBase: path.join(__dirname, './dist'),
-  publicPath: config.output.publicPath,
-  historyApiFallback: true,
-  hot: true,
-  stats: {
-    colors: true
-  }
-}).listen(4000, 'localhost', function (err) {
-  if (err) console.log(err);
-  console.log('Listening at localhost:4000');
-});
+  app.use(express.static(__dirname + '/dist'))
+
+  app.get('/', (req, res) => {
+    res.render('index')
+  })
+
+  app.listen(port, () => {
+    console.log('Our app is running on port ' + port)
+  })
+} else {
+  var path = require('path')
+  var webpack = require('webpack')
+  var WebpackDevServer = require('webpack-dev-server')
+  var config = require('./webpack.config')
+
+  new WebpackDevServer(webpack(config), {
+    contentBase: path.join(__dirname, './dist'),
+    publicPath: config.output.publicPath,
+    historyApiFallback: true,
+    hot: true,
+    stats: {
+      colors: true
+    }
+  }).listen(4000, 'localhost', function (err) {
+    if (err) console.log(err)
+    console.log('Listening at localhost:4000')
+})
+}
